c88dfb3e270919c57ff21183f52c46514cd2977b
seantowne <stowne@csumb.edu>
2017-02-07 06:20:37 +0000

    trying boto to connect to config vars

--- app.py
+++ app.py
@@ -2,14 +2,16 @@
 
 # app.y defines what to do when a request to see the index '/' page comes in.
 
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+
 import os
 import flask
 import random
 import gettyWrap
 import twitterWrap
 
-from boto.s3.connection import S3Connection
-s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+
 
 
 # Globals for theme selection
