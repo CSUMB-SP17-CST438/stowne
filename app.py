70d6b86aa296e17dcc4ad730eb41c3915e3c5be1
seantowne <stowne@csumb.edu>
2017-02-07 06:18:38 +0000

    trying boto to connect to config vars

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import random
 import gettyWrap
 import twitterWrap
 
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 
 
 # Globals for theme selection
